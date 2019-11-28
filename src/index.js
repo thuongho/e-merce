968379e05b3617b45321bb1ded6e7fc985d5133c
Thuong Ho <thuong.t.ho@gmail.com>
2019-11-27 18:58:55 -0800

    add react-router-dom add dynamic routing links to menu-item component using withRouter HOC

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  // browserrouter is a component we wrap around our app
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
