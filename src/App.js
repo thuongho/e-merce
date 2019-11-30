c36e76a22c01d4cdfe9813a916a497f85a2bcb19
Thuong Ho <thuong.t.ho@gmail.com>
2019-11-30 14:40:06 -0800

    convert App to Class to enable state add firebase auth subscription style sign in with google button

--- src/App.js
+++ src/App.js
@@ -6,18 +6,45 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    // subscribed to auth
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      console.log('user', user);
+      // store state of user
+      this.setState({ currentUser: user });
+    });
+  }
+
+  componentWillUnmount() {
+    // unsubscribe when the component is destroyed to prevent memory leak
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
