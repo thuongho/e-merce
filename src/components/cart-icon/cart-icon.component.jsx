defab275ddd7976b19757bffe3df08d2b12ed224
Thuong Ho <thuong.t.ho@gmail.com>
2019-12-06 16:14:01 -0800

    add cart selectors with memoized selectors update app with reselect

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.action';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
@@ -9,10 +10,10 @@ import './cart-icon.styles.scss';
 
 // functional component that renders a div
 // toggleCartHidden is now accessable cuz mapDispatchToProps has it as a method and it is passed into connect as a prop
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
     <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>0</span>
+    <span className='item-count'>{itemCount}</span>
   </div>
 );
 
@@ -21,4 +22,11 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+// const mapStateToProps = ({ cart: { cartItems } }) => ({
+//   itemCount: cartItems.reduce((totalItems, cartItem) => (totalItems + cartItem.quantity), 0)
+// });
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,18 @@
+import { createSelector } from 'reselect';
+
+// input selector
+const selectCart = state => state.cart;
+
+// by using createSelect to get the cartItems, cartItems is now a memoize item
+export const selectCartItems = createSelector(
+  // collection of input selectors
+  [selectCart],
+  // function return value out of selector
+  (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce((totalItems, cartItem) => (totalItems + cartItem.quantity), 0)
+);
