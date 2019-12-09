37e346ad1ca02748b3433d560a1d50e4ed7dd494
Thuong Ho <thuong.t.ho@gmail.com>
2019-12-08 19:23:24 -0800

    normalize shop collection data add selectCollection selector to return collection build out collection page with collection-item

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionItem from '../../components/collection-item/collection-item.component';
+import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ match }) => {
-  console.log('match', match.params.collectionId);
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
   return (
-  <div className='category'>
-    <h2>COLLECTION PAGE</h2>
+  <div className='collection-page'>
+    <h2 className='title'>{ title }</h2>
+    <div className='items'>
+      {
+        items.map(item => <CollectionItem key={item.id} item={item} />)
+      }
+    </div>
   </div>
 )};
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
