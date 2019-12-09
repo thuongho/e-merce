37ae2eb2230fd489e017fbb681c30704168b919c
Thuong Ho <thuong.t.ho@gmail.com>
2019-12-08 20:13:36 -0800

    add selectCollectionsForPreview to return collections in array format as the data is now normalized

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -10,18 +10,20 @@ import './collection-item.styles.scss';
 // change () to {} cuz we're executing multiple items in this expression
 const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
-  return (<div className='collection-item'>
-    <div
-      className='image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}></div>
-    <div className='collection-footer'>
-      <span className='name'>{name}</span>
-      <span className='price'>{price}</span>
+  return (
+    <div className='collection-item'>
+      <div
+        className='image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}></div>
+      <div className='collection-footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+      </div>
+      <CustomButton inverted onClick={() => addItem(item)}>Add to cart</CustomButton>
     </div>
-    <CustomButton inverted onClick={() => addItem(item)}>Add to cart</CustomButton>
-  </div>);
+  );
 };
 
 const mapDispatchToState = dispatch => ({
