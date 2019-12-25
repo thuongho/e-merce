15a68cfaa654ee805c8586fd388f60759667c764
Thuong Ho <thuong.t.ho@gmail.com>
2019-12-24 16:20:27 -0800

    update menu item component to use styled components

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import './menu-item.styles.scss';
+
+import {
+  MenuItemContainer,
+  BackgroundImageContainer,
+  ContentContainer,
+  TitleContainer,
+  SubtitleContainer
+} from './menu-item.styles';
 
 // destructure title from props
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
-  <div
-    className={`${size} menu-item`}
+  <MenuItemContainer
+    size={size}
     onClick={() => history.push(`${match.url}${linkUrl}`)}>
-    <div
+    <BackgroundImageContainer
       className='background-image'
-      style={{ backgroundImage: `url(${imageUrl})` }}>
-    </div>
-    <div className='content'>
-      <h1 className='title'>{ title.toUpperCase() }</h1>
-      <span className='subtitle'>SHOP NOW</span>
-    </div>
-  </div>
+      imageUrl={imageUrl}>
+    </BackgroundImageContainer>
+    <ContentContainer className='content'>
+      <TitleContainer>{ title.toUpperCase() }</TitleContainer>
+      <SubtitleContainer>SHOP NOW</SubtitleContainer>
+    </ContentContainer>
+  </MenuItemContainer>
 );
 
 export default withRouter(MenuItem);
